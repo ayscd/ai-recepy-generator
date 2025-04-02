fc6537076b87252fb93b5c02eab787c79cd1eff3
ayscd <ayeskacsta@gmail.com>
2025-04-01 23:12:19 -0300

    Update 4 - API and CSS
    - adjusting prompt and context
    - ajdusting .css file
    - trying to solve a bug

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 function displayRecepy(recepy) {
-  console.log(recepy.data);
-  console.log(recepy.data.answer);
+  let recepyWrapper = document.querySelector("#recepy");
+  recepyWrapper.classList.add("recepy-wrapper");
+
+  new Typewriter('#recepy', {
+    strings: recepy.data.answer,
+    autoStart: true,
+    delay: 10,
+    pauseFor: 700,
+    cursor: "",
+  });
 }
 
 function createRecepy(data) {
   data.preventDefault();
 
+  let recepy = document.querySelector("#recepy");
   let query = document.querySelector("#user-instructions").value;
   let apiKey = 'do8ae0f5a33a8b61b41f1t1ed44678b4';
-  let prompt = `User instructions = ${query}`;
-  let context = 'You are a professional brazillian cheff. Your job is to provide recepies according to the user instructions. DO NOT write a tittle to the recepy. You MUST consider the number of people eating before giving the recepy. The recepies presented must contain the ingredients (with quantities), the step by step for preparation, and the estimate time to prepare everything. The ingredients (with quantities) must be presented inside a <li></li> element. Te recepy must be divided with divs <div></div> according to each phase of preparation. All the titles of the phases should be written inside a <h3></h3> element. At the end of the recepy, sign with Bon appetit! inside a <strong></strong> element.';
+  let prompt = `User instructions = ${query}. Give me a complete recepy for this dish, with the ingredients (with quantities) and the step by step for preparation until conclusion. The ingredients (with quantities) must be presented inside a <li></li> element. Te recepy must be divided with divs <div></div> according to each phase of preparation. All the titles of the phases should be written inside a <h3></h3> element.`;
+  let context = 'You are a professional brazillian cheff. Your job is to provide whole recepies according to the user instructions. DO NOT write a tittle to the recepy. You MUST consider the number of people eating before giving the recepy. Be polite and objective in your instructions';
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`
 
-  console.log(apiUrl);
+  recepy.innerHTML = `<div class="generating"> ⏳ Generating a recepy of ${query}...</div>`;
   axios.get(apiUrl).then(displayRecepy);
 }
 
