393da7f52fa23e6fb7424f7dac36c53baf0c1005
ayscd <ayeskacsta@gmail.com>
2025-04-01 23:21:18 -0300

    END - final adjustments
    - ajdusting css

--- src/index.js
+++ src/index.js
@@ -17,8 +17,8 @@ function createRecepy(data) {
   let recepy = document.querySelector("#recepy");
   let query = document.querySelector("#user-instructions").value;
   let apiKey = 'do8ae0f5a33a8b61b41f1t1ed44678b4';
-  let prompt = `User instructions = ${query}. Give me a complete recepy for this dish, with the ingredients (with quantities) and the step by step for preparation until conclusion. The ingredients (with quantities) must be presented inside a <li></li> element. Te recepy must be divided with divs <div></div> according to each phase of preparation. All the titles of the phases should be written inside a <h3></h3> element.`;
-  let context = 'You are a professional brazillian cheff. Your job is to provide whole recepies according to the user instructions. DO NOT write a tittle to the recepy. You MUST consider the number of people eating before giving the recepy. Be polite and objective in your instructions';
+  let prompt = `User instructions = ${query}. Give me a recepy for this dish, with the ingredients (with quantities) and the steps for preparation until conclusion. Present the ingredients (with quantities) inside a <li></li> element. Te recepy must be divided with divs <div></div> according to each phase of preparation. All the titles of the phases should be written inside a <h3></h3> element.`;
+  let context = 'You are a professional brazillian cheff. Your job is to provide recepies according to the user instructions. DO NOT write a tittle to the recepy. You MUST consider the number of people eating before giving the recepy. Be polite. Make the prepare instructions short and objective';
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${encodeURIComponent(prompt)}&context=${encodeURIComponent(context)}&key=${apiKey}`
 
   recepy.innerHTML = `<div class="generating"> ⏳ Generating a recepy of ${query}...</div>`;
